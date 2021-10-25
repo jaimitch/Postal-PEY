d3948eaeef10cdec9c5a9b5fa7a673f16d21c05b
brisvn <brisvn@outlook.com>
2021-10-25 13:44:05 -0400

    removed double space from an input

--- src/Forms/Form3854.vue
+++ src/Forms/Form3854.vue
@@ -159,7 +159,7 @@
             <select class="to-input" v-model="formData.to">
                 <option value="APO AE 09459" selected>APO AE 09459</option>
 
-                <option value="AMT APO AE  09463">AMT APO AE  09463</option>
+                <option value="AMT APO AE 09463">AMT APO AE 09463</option>
                 <option value="AMF KENNEDY, NY 00300">AMF KENNEDY, NY  00300</option>
                 <option value="APO AE 09824">APO AE  09824</option>
                 <option value="APO AE 09459">APO AE  09459</option>
